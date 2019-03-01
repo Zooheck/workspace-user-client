a4192811dfdb596646181456f23558a899d94141
Zooheck <davidscottflack@gmail.com>
2019-02-28 19:15:23 -0600

    Implement logout functionality

--- app/src/components/NavBar/NavBar.js
+++ app/src/components/NavBar/NavBar.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+
+import { withRouter } from 'react-router-dom'
 
 import './NavBar.css'
 
-const NavBar = () => {
-  return (
-    <nav className="navbar">
-        <div className="link-container">
-            <NavLink to="/login">Log In</NavLink>
-            <NavLink to="/users">View Users</NavLink>
-            <button>Log Out</button>
-        </div>
-    </nav>
-  )
+class NavBar extends Component {
+  logOut = e => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    this.props.history.push('/login');
+  }
+  render() {
+    return (
+      <nav className="navbar">
+          <div className="link-container">
+              <NavLink to="/login">Log In</NavLink>
+              <NavLink to="/users">View Users</NavLink>
+              <button onClick={this.logOut}>Log Out</button>
+          </div>
+      </nav>
+    )
+  }
 }
 
-export default NavBar
+export default withRouter(NavBar)
